10ce3bf2e689cfb321780ed14a8b2373c731c232
Bruno DIAS <Bruno DIAS>
2017-08-31 06:20:47 -0300

    Alterando acesso a api

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
-export const API_URL = 'http://concilia-api.herokuapp.com/api/';
-export const STATIC_URL = 'http://concilia-api.herokuapp.com/static';
-export const BASE_URL = 'http://concilia-api.herokuapp.com';
+// export const API_URL = 'http://concilia-api.herokuapp.com/api/';
+// export const STATIC_URL = 'http://concilia-api.herokuapp.com/static';
+// export const BASE_URL = 'http://concilia-api.herokuapp.com';
 
-// export const API_URL = 'http://localhost:8000/api/';
-// export const STATIC_URL = 'http://localhost:8000/static';
-// export const BASE_URL = 'http://localhost:8000';
+export const API_URL = 'http://localhost:8000/api/';
+export const STATIC_URL = 'http://localhost:8000/static';
+export const BASE_URL = 'http://localhost:8000';
