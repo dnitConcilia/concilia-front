94ebf0edd6f10462883fb321eb5c2f79be1a1b3c
Bruno DIAS <Bruno DIAS>
2017-08-31 10:07:02 -0300

    Alterando endereço da api

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,6 +2,6 @@
 // export const STATIC_URL = 'http://concilia-api.herokuapp.com/static';
 // export const BASE_URL = 'http://concilia-api.herokuapp.com';
 
-export const API_URL = 'http://localhost:8000/api/';
-export const STATIC_URL = 'http://localhost:8000/static';
-export const BASE_URL = 'http://localhost:8000';
+export const API_URL = 'http://10.100.15.57:8080/api/';
+export const STATIC_URL = 'http://10.100.15.57:8080/static';
+export const BASE_URL = 'http://10.100.15.57:8080';
