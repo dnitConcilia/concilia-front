df7e20a55a9d5abc77a7ac950ca8b48218b27cdc
Biloco <biloco@debian-biloco.Biloco>
2017-09-14 23:41:47 -0300

    Mudando solicitação para servidor de produção

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,6 @@
-export const API_URL = 'http://localhost:8000/api/';
-export const STATIC_URL = 'http://localhost:8000/static';
-export const BASE_URL = 'http://localhost:8000';
-
+// export const API_URL = 'http://localhost:8000/api/';
+// export const STATIC_URL = 'http://localhost:8000/static';
+// export const BASE_URL = 'http://localhost:8000';
 
 // export const API_URL = 'http://concilia-api.herokuapp.com/api/';
 // export const STATIC_URL = 'http://concilia-api.herokuapp.com/static';
@@ -10,3 +9,7 @@ export const BASE_URL = 'http://localhost:8000';
 // export const API_URL = 'http://10.100.15.57:8080/api/';
 // export const STATIC_URL = 'http://10.100.15.57:8080/static';
 // export const BASE_URL = 'http://10.100.15.57:8080';
+
+export const API_URL = 'http://189.9.19.22:8080/api/';
+export const STATIC_URL = 'http://189.9.19.22:8080/static';
+export const BASE_URL = 'http://189.9.19.22:8080';
