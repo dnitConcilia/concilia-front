cf52d1d23cb3dca5d504ffc7dc2924619947a7b6
Donnici <donnici@fsociety.fsociety>
2017-10-23 17:04:54 -0200

    Inseção da página de documentação e alerta de teste

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -13,10 +13,11 @@ export class WarningComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
-		// setTimeout(function() {
-		// 	$(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");"shake"==o?$(".modal-dialog").velocity("callout."+o):"pulse"==o?$(".modal-dialog").velocity("callout."+o):"tada"==o?$(".modal-dialog").velocity("callout."+o):"flash"==o?$(".modal-dialog").velocity("callout."+o):"bounce"==o?$(".modal-dialog").velocity("callout."+o):"swing"==o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
-		// 	$('#warning').modal('show');
-		// }, 5500);
+		setTimeout(function() {
+			$(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");"shake"==o?$(".modal-dialog").velocity("callout."+o):"pulse"==o?$(".modal-dialog").velocity("callout."+o):"tada"==o?$(".modal-dialog").velocity("callout."+o):"flash"==o?$(".modal-dialog").velocity("callout."+o):"bounce"==o?$(".modal-dialog").velocity("callout."+o):"swing"==o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
+			
+			$('#warning').modal('show');
+		}, 5500);
 
 	}
 
