f9d861e0479de47c53f237e3fcb1697668fb3959
Donnici <donnici@fsociety.fsociety>
2017-10-24 10:35:56 -0200

    Funcionalidade de redirecionamento sem recarregar a página

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 declare var jquery: any;
 declare var $: any;
@@ -10,15 +11,22 @@ declare var $: any;
 })
 export class WarningComponent implements OnInit {
 
-	constructor() { }
+	constructor(
+		private router: Router
+	) { }
 
 	ngOnInit() {
 		setTimeout(function() {
 			$(".modal").each(function(l){$(this).on("show.bs.modal",function(l){var o=$(this).attr("data-easein");"shake"==o?$(".modal-dialog").velocity("callout."+o):"pulse"==o?$(".modal-dialog").velocity("callout."+o):"tada"==o?$(".modal-dialog").velocity("callout."+o):"flash"==o?$(".modal-dialog").velocity("callout."+o):"bounce"==o?$(".modal-dialog").velocity("callout."+o):"swing"==o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
-			
+
 			$('#warning').modal('show');
 		}, 5500);
 
+	}
+
+	public goTo(url: string): void {
+		$('#warning').modal('hide');
+		this.router.navigate([url]);
 	}
 
 }
