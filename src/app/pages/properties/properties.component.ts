0ec15227edbb17e79306b94b2de62f7047259aa8
Donnici <donnici@fsociety.fsociety>
2017-10-27 16:14:01 -0200

    Mapas na localização dos imóveis

--- src/app/pages/properties/properties.component.ts
+++ src/app/pages/properties/properties.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+declare var $: any;
+
 @Component({
 	selector: 'cba-properties',
 	templateUrl: './properties.component.html',
@@ -10,6 +13,17 @@ export class PropertiesComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
+		(function($){
+			$('.gallery').each(function() { // the containers for all your galleries
+				$(this).magnificPopup({
+					delegate: 'a', // the selector for gallery item
+					type: 'image',
+					gallery: {
+						enabled: true
+					}
+				});
+			});
+		})(jQuery);
 	}
 
 }
