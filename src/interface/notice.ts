6d616d38392037999cc7840874854fd88d7673b8
Donnici <donnici@fsociety.fsociety>
2017-10-27 15:47:16 -0200

    Formatando tabela de editais

--- src/interface/notice.ts
+++ src/interface/notice.ts
@@ -2,9 +2,9 @@ import { NoticeType } from './noticeType';
 
 export interface Notice {
 	id: number;
-	title: string;
 	description: string;
 	date: string;
+	publishDate: string;
 	notice: string;
 	noticeType: NoticeType;
 	link: string;
