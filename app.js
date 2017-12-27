eb256850bf3fad56594ceafc974c559ef65d3659
Donnici <donnici@fsociety.fsociety>
2017-12-27 17:22:53 -0200

    Bold nos entre títulos das notícias

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATH, DELETE');
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
   next();
  });
  
 app.get('*', (req, res) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	// res.header("Access-Control-Allow-Origin", "*");
+	// res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
