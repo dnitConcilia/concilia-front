39a340877a7da9476bb6f5115aec7d3d0d58b4b9
Bruno DIAS <Bruno DIAS>
2017-12-29 15:59:25 -0200

    alterações estruturais

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.all('*', function(req, res, next) {
-  response.header('Access-Control-Allow-Origin', '*');
-  response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATH, DELETE');
+  response.header('Access-Control-Allow-Oringin', '*');
+  response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
   next();
  });
  
